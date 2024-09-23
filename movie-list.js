09b969fd81558875fb313c0033382da6391ceab5
Ametrine <buitamman1231@gmail.com>
2024-09-23 12:38:21 +0700

    5

--- movie-list.js
+++ movie-list.js
@@ -16,6 +16,12 @@ const app = express();
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
+
+
 // Use session middleware
 app.use(session({
     secret: "abc",
